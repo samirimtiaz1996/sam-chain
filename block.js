354f68ab1294605c8cd61ab3f34afa7e3a883d08
Samir Imtiaz <samirimtiaz1234@gmail.com>
2019-11-26 21:32:15 +0600

    Blocks were successfully built and tested

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 =  require('crypto-js/sha256');
+
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp=timestamp;
@@ -10,7 +12,19 @@ class Block {
     timestamp : ${this.timestamp}
     lastHash  : ${this.lastHash.substring(0,10)}
     hash      : ${this.hash.substring(0,10)}
-    data      : ${this.data};`
+    data      : ${this.data}`;
+  }
+  static genesis(){
+    return new this('Default time','-----','hash1607073',[])
+  }
+  static mineBlock(lastBlock,data){
+    const timestamp = Date.now();
+    const lastHash  = lastBlock.hash;
+    const hash      = Block.hash(timestamp,lastHash,data)
+    return new this(timestamp,lastHash,hash,data)
+  }
+  static hash(timestamp,lastHash,data){
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
