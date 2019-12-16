c915d37419d44353fa01433b8cf0cd4db2785d5b
Samir Imtiaz <samirimtiaz1234@gmail.com>
2019-12-16 13:40:28 +0600

    MultiUser based app deployed,Blockchain network established
    Allows multiple user,peer to peer network,data sync

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
 const BlockChain = require('../blockchain');
+const P2pServer =require('./p2p-server');
 
-const HTTP_PORT =process.env.HTTP_PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app= express();
 const blockChain= new BlockChain();
+const p2pServer= new P2pServer(blockChain);
 
 app.use(bodyParser.json());
 
@@ -16,8 +18,9 @@ app.get('/blocks',(req,res)=> {
 app.post('/mine',(req,res)=>{
     const block = blockChain.addBlock(req.body.data);
     console.log(`New Block Added: ${block.toString()}`);
-
+    p2pServer.syncChains();
     res.redirect('/blocks');
 })
 
 app.listen(HTTP_PORT,()=> console.log(`listening on port ${HTTP_PORT}`));
+p2pServer.listen();
